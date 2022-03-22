7cc015234cead1ce8e41422784746d4ec26bb454
AndyZe <zelenak@picknik.ai>
2022-03-21 22:24:24 -0500

    Move robot config and description into this package

---
+++ abb_bringup/launch/abb_moveit.launch.py
@@ -0,0 +1,174 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+# TODO(andyz): add robot type arguments
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+    # Command-line arguments
+    robot_xacro_file = LaunchConfiguration("robot_xacro_file")
+    support_package = LaunchConfiguration("support_package")
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_xacro_file",
+            description="Xacro describing the robot.",
+            # TODO(andyz): add other options
+            choices=["irb1200_5_90.xacro"],
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "support_package",
+            description="Name of the support package",
+            # TODO(andyz): add other options
+            choices=["abb_irb1200_support"],
+        )
+    )
+
+    # Planning context
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(support_package), "urdf", robot_xacro_file]),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_description_semantic_config = load_file(
+        "abb_irb1200_5_90_moveit_config", "config/abb_irb1200_5_90.srdf"
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_config
+    }
+
+    kinematics_yaml = load_yaml(
+        "abb_irb1200_5_90_moveit_config", "config/kinematics.yaml"
+    )
+
+    # Planning Functionality
+    ompl_planning_pipeline_config = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/ResolveConstraintFrames default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        }
+    }
+    ompl_planning_yaml = load_yaml(
+        "abb_irb1200_5_90_moveit_config", "config/ompl_planning.yaml"
+    )
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    moveit_simple_controllers_yaml = load_yaml(
+        "abb_irb1200_5_90_moveit_config", "config/controllers.yaml"
+    )
+    moveit_controllers = {
+        "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+
+    trajectory_execution = {
+        "moveit_manage_controllers": True,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            kinematics_yaml,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
+        ],
+    )
+
+    # RViz
+    rviz_base = os.path.join(get_package_share_directory("abb_irb1200_5_90_moveit_config"), "rviz")
+    rviz_config = os.path.join(rviz_base, "moveit.rviz")
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            ompl_planning_pipeline_config,
+            kinematics_yaml,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            run_move_group_node,
+        ]
+    )
---
+++ abb_resources/CHANGELOG.rst
@@ -0,0 +1,19 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package abb_resources
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+1.3.1 (2019-09-17)
+------------------
+* Add Olive Green as a color (`#160 <https://github.com/ros-industrial/abb/issues/160>`_)
+* Contributors: Ryan DelGizzi, gavanderhoorn
+
+1.3.0 (2017-05-27)
+------------------
+* remove reference to source build and rebase to indigo devel.
+* kinetic-devel release of ros-industrial/abb
+* Contributors: Austin Deric
+
+1.2.1 (2017-03-27)
+------------------
+* Add abb_resources package from ros-industrial/abb_experimental@f3816a1
+* Contributors: Levi Armstrong
